5cac0c7f4ee15bedfa86cf3ebb8de1e877371e7a
Yasas <adamec@yasas.org>
2015-04-20 15:19:53 +0200

    Unit-tests cleanup.

--- xbase4j/src/test/java/org/yasas/xbase4j/UT_XBase.java
+++ xbase4j/src/test/java/org/yasas/xbase4j/UT_XBase.java
@@ -17,48 +17,26 @@
 package org.yasas.xbase4j;
 
 import org.junit.*;
-import org.yasas.xbase4j.api.meta.*;
 
 import java.io.*;
 import java.util.*;
 
-import static org.yasas.xbase4j.XBase.*;
-
 public class UT_XBase {
   @Test
   public void testOpen() throws IOException {
-//      new File("/home/yasas/Projects/T.E.A.S/Caris.8 [Misc]/importy/data/PscCZ.dbf")
-
-//      new File("h:\\Home\\Projects\\Personal [Java]\\Live\\xbase4j-2.0\\xbase4j\\src\\main\\resources\\org\\yasas\\xbase4j\\files\\simple_01.dbf")
-
-//      new File("h:\\Home\\Projects\\T.E.A.S [Local]\\Data.Zkusebni\\WorksTeas\\data\\Salon\\SkladSal\\auta.dbf")
-//      new File("h:\\Home\\Projects\\T.E.A.S [Local]\\Data.Zkusebni\\WorksTeas\\data\\Salon\\SkladSal\\autaar.dbf")
-
-//      new File("h:\\Home\\Projects\\T.E.A.S [Local]\\Data.Dojacek\\vozidla\\vozid_ev.dbf")
-//      new File("h:\\Home\\Projects\\T.E.A.S [Local]\\Data.Dojacek\\sklad_a\\_sklad.dbf")
-//      new File("h:\\Home\\Projects\\T.E.A.S [Local]\\Data.Dojacek\\sklad_a\\_skarty.dbf")
-//      new File("/home/yasas/Projects/T.E.A.S/Caris.8 [Misc]/data/Data.Dojacek/Zakaznik/zakaz_ev.dbf")
-//      new File("h:\\Home\\Projects\\T.E.A.S [Local]\\Data.Dojacek\\doklrady\\drcit__d.dbf")
-
-    final XBaseFile xBaseFile = new XBase()/*.withLanguage(Language.DosEastEurope)*/.open(
+    final XBaseFile xBaseFile = new XBase().open(
       new File("/home/yasas/Projects/T.E.A.S/Caris.8 [Misc]/importy/data/zv_pcobc_copy.dbf")
     );
 
     try {
-      for (int i = 12; i < 100/*xBaseFile.rowCount()*/; i++) {
+      for (int i = 0; i < 100/*xBaseFile.rowCount()*/; i++) {
         if (xBaseFile.go(i)) {
-//          System.out.println(xBaseFile.getValue("v_spz"));
           final Map<String, Object> map = xBaseFile.scatterAsMap(); {
             map.put("NAZPOST", "Posta " + Integer.toString(i));
 
             xBaseFile.gatherAsMap(map);
           }
           System.out.println(map);
-
-//          xBaseFile.evaluateTemplate("insert into dbo.vozidlo (v_id, v_spz, v_vin) values (@{v_id}, '@{v_spz}', '@{v_vin}')");
-//          xBaseFile.evaluateTemplate("insert into dbo.sklad (sklad_id) values (@{sklad_id})");
-//          System.out.println(xBaseFile.evaluateTemplate("insert into dbo.sklad (sklad_id) values (@{sklad_id})"));
-//          System.out.println(xBaseFile.evaluate("sjkpov + ': ' + (poloz_id + scena)"));
         }
       }
     } catch (Exception e) {
@@ -68,16 +46,16 @@ public class UT_XBase {
     }
   }
 
-  @Test
-  public void testCreate() throws IOException {
-    new XBase()
-      .with(Language.WinEastEurope, Version.VisualFoxPro)
-      .withFields(fieldBuilder(Version.VisualFoxPro)
-        .N("sklad_id", 2, 0)
-        .I("poloz_id")
-        .C("scislo", 20)
-        .C("snazev", 50)
-        .build())
-      .create(new File("/home/yasas/Projects/T.E.A.S/Caris.8 [Misc]/importy/data/"));
-  }
+//  @Test
+//  public void testCreate() throws IOException {
+//    new XBase()
+//      .with(Language.WinEastEurope, Version.VisualFoxPro)
+//      .withFields(fieldBuilder(Version.VisualFoxPro)
+//        .N("sklad_id", 2, 0)
+//        .I("poloz_id")
+//        .C("scislo", 20)
+//        .C("snazev", 50)
+//        .build())
+//      .create(new File("/home/yasas/Projects/T.E.A.S/Caris.8 [Misc]/importy/data/"));
+//  }
 }
